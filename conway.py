3e55e1e22fada3934a06fd4a65769f43207063ef
adanvers <alexander.danvers@gmail.com>
2022-02-18 18:15:39 -0500

    created pure python (not ipynb) file

---
+++ conway.py
@@ -0,0 +1,22 @@
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# create a pure python array (list of lists)
+
+# update world function
+
+grid = []
+
+height = 20
+width = 20
+
+for i in range(height):
+    grid.append(["*"]*width)
+
+# simulate world
+
+# could use a web front end, turn this into an API
+# function that updates matrix could be an API
+
+for l in grid:
+    print(l)
